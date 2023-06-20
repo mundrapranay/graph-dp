05449117bda867f88f2412922c5ba40053151e67
Pranay Mundra <pranaymundra4510@gmail.com>
2023-06-20 21:34:06 +0200

    new approach

--- src/distributed_graph_algorithm.cpp
+++ src/distributed_graph_algorithm.cpp
@@ -1,79 +1,79 @@
+#include <mpi.h>
 #include <iostream>
 #include <vector>
-#include <mpi.h>
 
-// Function to perform computation on a node's adjacency list
-int computeNodeValue(const std::vector<int>& adjacencyList) {
-    // Perform computation on the adjacency list and return the computed value
-    int sum = 0;
-    for (int neighbor : adjacencyList) {
-        // Compute the sum of neighbors (example computation)
-        sum += neighbor;
-    }
-    return sum;
+// Function to perform computation on a node and its adjacency list
+int computeNode(int node, const std::vector<int>& adjacencyList) {
+    // TODO: Implement your computation logic here
+    // This is just a placeholder
+    return node * adjacencyList.size();
 }
 
 int main(int argc, char** argv) {
+    // Initialize MPI
     MPI_Init(&argc, &argv);
 
-    int worldSize, rank;
-    MPI_Comm_size(MPI_COMM_WORLD, &worldSize);
+    // Get the total number of processes and the rank of the current process
+    int numProcesses, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    if (rank == 0) {
-        // Master node (rank 0) code
-        std::vector<std::vector<int>> graph = {
-            {1, 2},     // Node 0 adjacency list
-            {0, 3},     // Node 1 adjacency list
-            {0, 3},     // Node 2 adjacency list
-            {1, 2, 4},  // Node 3 adjacency list
-            {3, 5},     // Node 4 adjacency list
-            {4}         // Node 5 adjacency list
-        };
-
-        int numNodes = graph.size() - 1;  // Number of worker nodes
-        int nodesPerWorker = numNodes / worldSize;
-        int extraNodes = numNodes % worldSize;
-
-        // Distribute work to worker nodes
-        for (int dest = 1; dest <= numNodes; dest++) {
-            int numNodesToProcess = nodesPerWorker + (dest <= extraNodes ? 1 : 0);
-            MPI_Send(&numNodesToProcess, 1, MPI_INT, dest, 0, MPI_COMM_WORLD);
-            for (int i = 0; i < numNodesToProcess; i++) {
-                MPI_Send(graph[dest].data(), graph[dest].size(), MPI_INT, dest, 0, MPI_COMM_WORLD);
-            }
-        }
-
-        // Receive computed values from worker nodes
-        std::vector<int> allNodeValues(numNodes + 1);
-        for (int src = 1; src <= numNodes; src++) {
-            MPI_Recv(&allNodeValues[src], 1, MPI_INT, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
-
-        // Output the computed values
-        std::cout << "Computed values:" << std::endl;
-        for (int i = 0; i <= numNodes; i++) {
-            std::cout << "Node " << i << ": " << allNodeValues[i] << std::endl;
-        }
-    } else {
-        // Worker nodes code
-        int numNodesToProcess;
-        MPI_Recv(&numNodesToProcess, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-        MPI_Probe(0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        int receivedCount;
-        MPI_Get_count(MPI_STATUS_IGNORE, MPI_INT, &receivedCount);
-
-        std::vector<int> localAdjacencyList(receivedCount);
-        MPI_Recv(localAdjacencyList.data(), receivedCount, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-        // Perform computation on the local adjacency list
-        int nodeValue = computeNodeValue(localAdjacencyList);
-
-        // Send computed value back to the master node
-        MPI_Send(&nodeValue, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    if (numProcesses < 2) {
+        std::cerr << "Error: At least 2 processes are required." << std::endl;
+        MPI_Finalize();
+        return 1;
     }
 
+    // Assuming the graph is represented as an adjacency list
+    // Each process will receive a portion of the adjacency list
+    std::vector<int> adjacencyList;
+    if (rank == 0) {
+        // Root process (rank 0) reads the graph and distributes it
+        // Assuming the graph is read from a file or some other source
+        // Modify this section as per your graph representation
+
+        // Example: Create a sample adjacency list
+        adjacencyList = {
+            {1, 2, 3, 4},
+            {0, 2, 3, 4},
+            {0, 1, 3, 4},
+            {0, 1, 2, 4},
+            {0, 1, 2, 3}
+        };
+    }
+
+    // Broadcast the size of the adjacency list to all processes
+    int adjacencyListSize = adjacencyList.size();
+    MPI_Bcast(&adjacencyListSize, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // Each process allocates memory for its portion of the adjacency list
+    std::vector<int> localAdjacencyList(adjacencyListSize);
+
+    // Scatter the adjacency list from root process to all processes
+    MPI_Scatter(adjacencyList.data(), adjacencyListSize, MPI_INT,
+                localAdjacencyList.data(), adjacencyListSize, MPI_INT,
+                0, MPI_COMM_WORLD);
+
+    // Perform computation on the local portion of the graph
+    int result = computeNode(rank, localAdjacencyList);
+
+    // Gather the results from all processes to the root process
+    std::vector<int> results(numProcesses);
+    MPI_Gather(&result, 1, MPI_INT,
+               results.data(), 1, MPI_INT,
+               0, MPI_COMM_WORLD);
+
+    // Print the results at the root process
+    if (rank == 0) {
+        std::cout << "Results: ";
+        for (int i = 0; i < numProcesses; ++i) {
+            std::cout << results[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    // Finalize MPI
     MPI_Finalize();
+
     return 0;
 }
