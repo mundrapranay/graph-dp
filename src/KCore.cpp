956b75c90c93d1021736259b7927ed9f5257c185
Pranay Mundra <pranaymundra4510@gmail.com>
2023-06-27 09:30:10 +0200

    LDS implemented

--- src/KCore.cpp
+++ src/KCore.cpp
@@ -8,7 +8,7 @@
 // using namespace std;
 // using namespace LDS;
 
-
+namespace distributed_kcore{
 
 
 std::vector<double> KCore(std::vector<std::vector<int>> adjacencyLists, double nu, double epsilon) {
@@ -16,9 +16,11 @@ std::vector<double> KCore(std::vector<std::vector<int>> adjacencyLists, double n
     double phi = 0.5;
     double lambda = (2/9) * (2 * nu - 5);
     double delta = 9.0;
-    LDS::LDS *lds = new LDS::LDS(adjacencyLists.size(), epsilon, delta, false);
+    LDS *lds = new LDS(adjacencyLists.size(), epsilon, delta, false);
     
 
 
     return core_numbers;
 }
+
+} // end of namespace distributed_kcore
