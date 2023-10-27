53eb6c069f9df2c9914c003ed87854cd6f6273e6
Pranay Mundra <pranaymundra4510@gmail.com>
2023-10-27 14:25:31 -0400

    partitioning_works

--- benchmark.py
+++ benchmark.py
@@ -87,7 +87,7 @@ def run_benchmark_partition():
         # for bias in [0, 1]:
         for bias in [1]:
             for factor_id in range(5):
-                for bias_factor in range(1, 5):
+                for bias_factor in range(1, 21):
                     output_file = f'/home/ubuntu/results_new/graph_{graph}_factor_id_{factor_id}_bias_{bias}_bias_factor_{bias_factor}_partitioned.txt'
                     if not os.path.exists(output_file):
                         cmd = [
