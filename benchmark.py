69016a8a162f6ea5f0594778ee2ad7a7d4000800
Pranay Mundra <pranaymundra4510@gmail.com>
2023-12-01 14:53:44 -0500

    dev_testing_single_file

--- benchmark.py
+++ benchmark.py
@@ -51,7 +51,7 @@ def run_benchmark():
                             'mpirun',
                             '-np', str(num_processes),
                             './build/DistributedGraphAlgorithm',
-                            f'./graphs/{graph}.txt',
+                            f'./graphs/{graph}',
                             str(eta), str(epsilon), str(phi),
                             str(factor_id), str(bias), str(bias_factor), str(GRAPH_SIZES[graph])
                         ]
