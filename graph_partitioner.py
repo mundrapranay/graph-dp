ecc8cfdd791dce857ad5535115eb7f6bdfb55157
Pranay Mundra <pranaymundra4510@gmail.com>
2024-02-08 13:18:25 -0500

    Update graph_partitioner.py

--- graph_partitioner.py
+++ graph_partitioner.py
@@ -14,6 +14,7 @@ GRAPH_SIZES = {
     'gplus' : 107614,
     'imdb' : 896308,
     'random_gen' : 1000
+    'random_gen_2' : 2500
 }
 
 
@@ -126,10 +127,10 @@ def check_graphs_reads(graph):
 
 
 if __name__ == '__main__':
-    partition_graph('Gplus', 17)
-    partition_graph('IMDB', 17)
-    # generate_complete_graph(1000, './graphs/random_gen')
-    # partition_graph('random_gen', 17)
+    # partition_graph('Gplus', 17)
+    # partition_graph('IMDB', 17)
+    generate_complete_graph(2500, './graphs/random_gen_2')
+    partition_graph('random_gen_2', 17)
 #   partition_graph('zhang_dblp', 17)
 #    partition_graph('zhang_orkut', 17)
     #check_graphs_reads('zhang_dblp')
